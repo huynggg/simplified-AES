b8b098969c61ea7228ec6a237d1f2706e49cbd02
huynggg <huyanguyen3695@gmail.com>
2021-11-09 08:58:41 -0600

    Revised part c -padding
    new padding function:
    - Append "A" to the previous string instead of just printing it out to the screen
    - Added printing/saving function for padding

--- aes.py
+++ aes.py
@@ -6,7 +6,7 @@
 
 import re
 
-# Read in input file
+# Read in input file.
 def read_data(file_name):
 	
 	file = open(file_name, "r")
@@ -15,7 +15,7 @@ def read_data(file_name):
 	# Convert list to int
 	return content
 
-# Write to an output file
+# Write to an output file.
 def write_data(content, file_name):
 
 	file = open(file_name, "a")
@@ -23,13 +23,13 @@ def write_data(content, file_name):
 		file.write(element)
 	file.close()
 
-# Proprocessing function
+# Proprocessing function.
 def pre_processing(content):
 	result = re.sub(r'[^\w\s]', '', content)
 	result = re.sub(r'\s+', '', result)
 	return result
 
-# Generate key for Vigenère cipher
+# Generate a key for Vigenère Cipher.
 def generate_key(content, key):
     processed_key = []
     key_length = len(key)
@@ -44,7 +44,7 @@ def generate_key(content, key):
     return "".join(processed_key)
 
 
-# Substitution using Vigenère cipher
+# Substitution using Vigenère Cipher.
 def substitution(content, key):
     cipher_text = []
     for i in range(len(content)):
@@ -54,33 +54,36 @@ def substitution(content, key):
         cipher_text.append(chr(x))
     return("" . join(cipher_text))
 
-# Padding & Write to file
-def padding(content, file_name):
-	file = open(file_name, "a")
-	file.write("\n\nPadding:\n")
-
+# Padding function.
+def padding(content):
 	if (content != None) and len(content):
 		reminder = len(content) % 16
 		pads = 16 - reminder
 		for i in range(len(content) + pads):
-			if i < len(content):
-				print(content[i] + "", end='')
-				file.write(content[i] + "")
-			else:
-				print("A", end='')
-				file.write("A")
+			if i >= len(content):
+				content += "A"
+	return content
 
-			if (i+1) % 4 == 0:
-				print("")
-				file.write("" + "\n")
-			if (i+1) % 16 == 0:
-				print("\n", end='')
-				file.write("\n")
+# Function to print the padded string to the screen,
+# and write it to the output file.
+def print_padded(content, file_name):
+	file = open(file_name, "a")
+	file.write("\n\nPadding:\n")
+	for i in range(len(content)):
+		if i < len(content):
+			print(content[i] + "", end='')
+			file.write(content[i] + "")
+		if (i+1) % 4 == 0:
+			print("")
+			file.write("" + "\n")
+		if (i+1) % 16 == 0:
+			print("\n", end='')
+			file.write("\n")
 
 
 # Driver code
 def driver_code():
-	# variables to save user's input
+	# variables to save user's inputs
 	input_filename = input("Enter the name of the input plaintext file: ")
 	key_filename = input("Enter the name of the input key file: ")
 	output_filename = input("Enter the name of the output ciphertext file: ")
@@ -90,22 +93,25 @@ def driver_code():
 
 	# Part a - Preprocessing
 	processed_content = pre_processing(input_content) # save input after processing
-	print(f'Preprocessing:\n{processed_content}')
+	print(f'Preprocessing:\n{processed_content}') # Print to the screen
 
 	result = "Preprocessing:\n" + processed_content # temporary variable to save the result
-	write_data(result, output_filename) # save to output
+	write_data(result, output_filename) # save to output file
 
 	# Part b - Substitution
 	vigen_key = generate_key(processed_content, key_content)
 	subs_content = substitution(processed_content, vigen_key)
-	print(f'\nSubstitution:\n{subs_content}')
+	print(f'\nSubstitution:\n{subs_content}') # Print to the screen
 
 	result = "\n\nSubstitution:\n" + subs_content # temporary variable to save the result
-	write_data(result, output_filename) # save to output
+	write_data(result, output_filename) # save to output file
+	print(type(subs_content))
 
 	# Part c - Padding
+	padded_content = padding(subs_content)
 	print(f'\nPadding:')
-	padding(subs_content, output_filename)
+	print_padded(padded_content, output_filename) # Print to the screen & save to file
+
 
 # Main
 if __name__ == '__main__':
